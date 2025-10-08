484eac5e007e2343cb6f4e09d6940b0407db32be
Arthur-AZ-Zhou <arthuracct@gmail.com>
2025-10-08 11:43:10 -0700

    Add mux definition

--- Lab1 Will Arthur/Lab1 Will Arthur.srcs/sources_1/new/lab1.v
+++ Lab1 Will Arthur/Lab1 Will Arthur.srcs/sources_1/new/lab1.v
@@ -19,11 +19,65 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module m21(
+module m2_1(
     input D0,
     input D1,
     input S,
     output Y
     );
+    wire T1, T2, Sbar;
+    and (T1, D1, S); // T1 = D1 && S
+    and (T2, D0, Sbar); // T2 = D0 && Sbar
+    not (Sbar, S); // Sbar = ~S
+    or (Y, T1, T2); // Y = T1 | T2
+    // Note that the sequence of instructions are not synchronous.
 endmodule
+
+module m3_1(
+        input D0,
+        input D1,
+        input D2,
+        input S[0:1],
+        output Y
+        );
+        wire T1;
+        m2_1(T1, D0, D1, S[0]);
+        m2_1(Y, T1, D2, S[1]);    
+endmodule
+
+module m4_1(
+        input D[0:3],
+        input S[0:1],
+        output Y
+        );
+        wire T1;
+        wire T2;
+        m2_1(T1, D[0], D[1], S[0]);
+        m2_1(T2, D[2], D[3], S[0]);
+        m2_1(Y, T1, T2, S[1]);
+endmodule
+
+module m8_1(
+        input wire D[0:7],
+        input wire S[0:2],
+        output Y
+        );
+        wire T1;
+        wire T2;
+        m4_1(T1, D[0:3], S[0:1]);
+        m4_1(T2, D[4:7], S[0:1]);
+        m2_1(Y, T1, T2, S[2]);
+endmodule
+
+module m16_1(
+        input D[16],
+        input S[4],
+        output Y
+        );
+        wire T1;
+        wire T2;
+        m81(T1, D[0:7], S[0:2]);
+        m81(T2, D[8:15], S[0:2]);
+        m21(Y, T1, T2, S[3]);
+endmodule
+
