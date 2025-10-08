a971d9f3e523fb3b73e539acbd65dd27ef59dae2
Arthur-AZ-Zhou <arthuracct@gmail.com>
2025-10-08 11:12:45 -0700

    Add skeleton for lab1

---
+++ Lab1 Will Arthur/Lab1 Will Arthur.srcs/sources_1/new/lab1.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/08/2025 11:12:24 AM
+// Design Name: 
+// Module Name: m21
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module m21(
+    input D0,
+    input D1,
+    input S,
+    output Y
+    );
+endmodule
