239dba341b5f416b50f8fac3ceedd150e15209b2
Arthur-AZ-Zhou <arthuracct@gmail.com>
2025-10-13 11:33:10 -0700

    pushing changes so jimmy can look at registers

--- Lab1 Will Arthur/Lab1 Will Arthur.srcs/sources_1/new/lab1.v
+++ Lab1 Will Arthur/Lab1 Will Arthur.srcs/sources_1/new/lab1.v
@@ -87,3 +87,37 @@ module alu_1bit(
         );
         
 endmodule; 
+
+module reg16(
+        input clk,
+        input rst,
+        input [4:0] Ra,
+        input [4:0] Rb,
+        input [4:0] Rw,
+        input WrEn,
+        input [15:0] busW,
+        output [15:0] busA,
+        output [15:0] busB
+        );
+        
+        //[0:31] array of 16 bit registers
+        reg [15:0] myregs [0:31];
+        
+        integer i;
+        
+        always @(posedge clk or posedge rst) begin
+            if (rst) begin
+                for (i = 0; i < 32; i = i + 1) begin
+                    myregs[i] <= 0; //set all to 0
+                end
+            end else begin
+                if (WrEn) begin
+                    myregs[Rw] <= busW; //write busW to Rw specified register
+                end
+            end
+        end
+        
+        //assign to 2^5 = 32 specific register indicated by Ra/Rb (read reflects NEW VALUE IF r/w same time)
+        assign busA = myregs[Ra];
+        assign busB = myregs[Rb];
+endmodule;
