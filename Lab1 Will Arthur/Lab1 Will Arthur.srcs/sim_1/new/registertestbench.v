a071129b46ba94f6e18fc96010e4dafd5774afd5
Arthur-AZ-Zhou <arthuracct@gmail.com>
2025-10-15 11:51:40 -0700

    Complete test bench & add ALU implementation

--- Lab1 Will Arthur/Lab1 Will Arthur.srcs/sim_1/new/registertestbench.v
+++ Lab1 Will Arthur/Lab1 Will Arthur.srcs/sim_1/new/registertestbench.v
@@ -42,21 +42,24 @@ module registertestbench;
         .busB(busB)
     );
     
+    integer i = 0;
     initial begin;
         // Test 1: Write to a register and then read it
-        rst = 0;
+        rst = 1;
         clk = 0;
         WrEn = 1;
         busW = 16'h5A8B;
-        Rw = 5'b00001;
+        Rw = 5'b00000;
         #10;
         clk = 1;
+        rst = 0;
         #10;
         clk = 0;
-        Ra = 5'b00001;
+        Ra = 5'b00000;
         #10; // busA here should be 5A8B
         clk = 1;
-        
+
+        #50; // clock break        
         // Reset
         rst = 1;
         clk = 0;
@@ -65,11 +68,11 @@ module registertestbench;
         rst = 0;
         #10;
         clk = 0;
-        #100; // clock break
+        #50; // clock break        
         
         // Test 2: Write to two registers and read from them simultaneously
         WrEn = 1;
-        busW = 16'h5A8B;
+        busW = 16'h158B;
         Rw = 5'b00001;
         #10;
         clk = 1;
@@ -85,7 +88,9 @@ module registertestbench;
         Rb = 5'b00010;
         clk = 1;
         #10; // busA here should be 5A8B, busB here should be 000C
+        clk = 0;
 
+        #50; // clock break        
         // Reset
         rst = 1;
         clk = 0;
@@ -94,7 +99,98 @@ module registertestbench;
         rst = 0;
         #10;
         clk = 0;
-        #100; // clock break
+        #50; // clock break        
+
+        // Test 3: Check reset priority
+        rst = 1;
+        clk = 0;
+        WrEn = 1;
+        busW = 16'h2A8B;
+        Rw = 5'b00000;
+        Ra = 5'b00000;
+        // Wait for a few cycles
+        for (i=0; i<10; i=i+1) begin
+            clk = 0;
+            #10;
+            clk = 1;
+            #10;
+        end
+         
+        #50; // clock break        
+        // Reset
+        rst = 1;
+        clk = 0;
+        #10;
+        clk = 1;
+        rst = 0;
+        #10;
+        clk = 0;
+        #50; // clock break  
+        
+        // Test 4: Check register persistence
+        
+        // Write into registers 0, 1, 2, 3
+        Ra = 5'b11111;
+        Rb = 5'b11111;
+        Rw = 5'b00000;
+        busW = 16'h10A0;
+        #10;
+        clk = 1;
+        #10;
+        clk = 0;
+        Rw = 5'b00001;
+        busW = 16'h10B0;
+        #10;
+        clk = 1;
+        #10;
+        clk = 0;
+        Rw = 5'b00010;
+        busW = 16'h10C0;
+        #10;
+        clk = 1;
+        #10;
+        clk = 0;
+        Rw = 5'b00011;
+        busW = 16'h10D0;
+        #10;
+        clk = 1;
+        #10;
+        clk = 0;
+ 
+        WrEn = 0;
+        #10;
+        clk = 1;
+        #10;
+        clk = 0;
+        
+        // Read from registers 0, 1
+        Ra = 5'b00000;
+        Rb = 5'b00001;
+        clk = 1;
+        #50; // clock break 
+        clk = 0;
+        #10;
+        
+        // Read from registers 2, 3
+        Ra = 5'b00010;
+        Rb = 5'b00011;
+        clk = 1;
+        #50; 
+        clk = 0;
+        #10;
+            
+        // Now reset - check that in both cases
+        // registers contain 0
+        rst = 1;
+        clk = 1;
+        #50;
+        
+        clk = 0;
+        Ra = 5'b00000;
+        Rb = 5'b00001;
+        #10;
+        clk = 1;
+        #50;
         
         $finish;
     end
---
+++ Lab1 Will Arthur/Lab1 Will Arthur.srcs/sources_1/new/alu.v
@@ -0,0 +1,538 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/01/2025 11:13:45 AM
+// Design Name: 
+// Module Name: addbit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+    input [15:0] a,
+    input [15:0] b,
+    input [3:0] ctrl,
+    output [15:0] s,
+    output zero,
+    output overflow
+);
+
+    wire [15:0] add_out, sub_out, or_out, and_out, dec_out, inc_out, inv_out;
+    wire [15:0] ashiftl_out, ashiftr_out, lshift_out, rshift_out;
+    wire sle_bit;
+
+    wire add_cout, sub_cout;
+    adder16 add_unit (.a(a), .b(b), .cin(0), .sum(add_out), .cout(add_cout));
+    subtract16 sub_unit (.a(a), .b(b), .out(sub_out));
+
+    or16 or_unit (.a(a), .b(b), .out(or_out));
+    and16 and_unit (.a(a), .b(b), .out(and_out));
+
+    increment inc_unit (.a(a), .out(inc_out));
+    decrement dec_unit (.a(a), .out(dec_out));
+    invert16 inv_unit (.a(a), .out(inv_out));
+
+    lshiftby lshift_unit (.a(a), .b(b), .out(lshift_out));
+    rshiftby rshift_unit (.a(a), .b(b), .arithmetic(1'b0), .out(rshift_out));
+
+    lshiftby ashiftl_unit (.a(a), .b(b), .out(ashiftl_out));
+    rshiftby ashiftr_unit (.a(a), .b(b), .arithmetic(1'b1), .out(ashiftr_out));
+
+    sle16 sle_unit (.a(a), .b(b), .out(sle_bit));
+    wire [15:0] sle_out = {15'b0, sle_bit};
+
+    mux12to1_16bit result_mux (
+        .in0 (sub_out), // 0000
+        .in1 (add_out), // 0001
+        .in2 (or_out), // 0010
+        .in3 (and_out), // 0011
+        .in4 (dec_out), // 0100
+        .in5 (inc_out), // 0101
+        .in6 (inv_out), // 0110
+        .in7 (ashiftl_out), // 1100
+        .in8 (ashiftr_out), // 1110
+        .in9 (lshift_out), // 1000
+        .in10(rshift_out), // 1010
+        .in11(sle_out), // 1001
+        .sel(ctrl),
+        .out(s)
+    );
+
+    // zero flag
+    assign zero = (s == 16'b0) ? 1'b1 : 1'b0;
+
+    // overflow flag for arithmetic shifts when sign bit changed
+    wire a_sign = a[15];
+    wire ashiftl_sign = ashiftl_out[15];
+    wire ashiftr_sign = ashiftr_out[15];
+
+    wire ovf_ashiftl = (ctrl == 4'b1100) && (a_sign != ashiftl_sign);
+    wire ovf_ashiftr = (ctrl == 4'b1110) && (a_sign != ashiftr_sign);
+
+    assign overflow = ovf_ashiftl | ovf_ashiftr;
+
+endmodule
+
+
+
+module m2_1(
+    input D0,
+    input D1,
+    input S,
+    output Y
+    );
+    wire T1, T2, Sbar;
+    and (T1, D1, S); // T1 = D1 && S
+    and (T2, D0, Sbar); // T2 = D0 && Sbar
+    not (Sbar, S); // Sbar = ~S
+    or (Y, T1, T2); // Y = T1 | T2
+    // Note that the sequence of instructions are not synchronous.
+endmodule
+
+module m2_1(
+    input D0,
+    input D1,
+    input S,
+    output Y
+);
+    wire T1, T2, Sbar;
+    and (T1, D1, S);
+    not (Sbar, S);
+    and (T2, D0, Sbar);
+    or  (Y, T1, T2);
+endmodule
+
+module m4_1(
+    input [0:3] D,
+    input [0:1] S,
+    output Y
+);
+    wire T1, T2;
+    m2_1 u1 (.D0(D[0]), .D1(D[1]), .S(S[0]), .Y(T1));
+    m2_1 u2 (.D0(D[2]), .D1(D[3]), .S(S[0]), .Y(T2));
+    m2_1 u3 (.D0(T1), .D1(T2), .S(S[1]), .Y(Y));
+endmodule
+
+module m8_1(
+    input [0:7] D,
+    input [0:2] S,
+    output Y
+);
+    wire T1, T2;
+    m4_1 u1 (.D(D[0:3]), .S(S[0:1]), .Y(T1));
+    m4_1 u2 (.D(D[4:7]), .S(S[0:1]), .Y(T2));
+    m2_1 u3 (.D0(T1), .D1(T2), .S(S[2]), .Y(Y));
+endmodule
+
+module m16_1(
+    input [0:15] D,
+    input [0:3] S,
+    output Y
+);
+    wire T1, T2;
+    m8_1 u1 (.D(D[0:7]), .S(S[0:2]), .Y(T1));
+    m8_1 u2 (.D(D[8:15]), .S(S[0:2]), .Y(T2));
+    m2_1 u3 (.D0(T1), .D1(T2), .S(S[3]), .Y(Y));
+endmodule
+
+
+module mux12to1_16bit (
+    input  [15:0] in0,
+    input  [15:0] in1,
+    input  [15:0] in2,
+    input  [15:0] in3,
+    input  [15:0] in4,
+    input  [15:0] in5,
+    input  [15:0] in6,
+    input  [15:0] in7,
+    input  [15:0] in8,
+    input  [15:0] in9,
+    input  [15:0] in10,
+    input  [15:0] in11,
+    input  [3:0] sel,
+    output [15:0] out
+);
+    genvar i;
+    generate
+        for (i = 0; i < 16; i = i + 1) begin : mux_bitwise
+            wire [15:0] bit_slice;
+            assign bit_slice = {
+                16'b0, 16'b0,16'b0, 16'b0,
+                in11[i], in10[i], in9[i], in8[i],
+                in7[i], in6[i], in5[i], in4[i],
+                in3[i], in2[i], in1[i], in0[i]
+            };
+
+            m16_1 u_mux (
+                .D(bit_slice),
+                .S(sel),
+                .Y(out[i])
+            );
+        end
+    endgenerate
+endmodule
+
+
+
+// not allowed to use builtin XOR
+module custom_xor( 
+    output y,
+    input a,
+    input b
+    );
+    
+    wire notA, notB, AAndNotB, NotAAndB;
+    not (notA, a);
+    not (notB, b);
+    and (AAndNotB, a, notB);
+    and (NotAAndB, notA, b);
+    or (y, AAndNotB, NotAAndB);
+endmodule    
+    
+module adder(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+    );
+    
+    wire AXorB, AAndB, AXorB_And_Cin;
+    
+    custom_xor x1 (.y(AXorB), .a(a), .b(b));
+    custom_xor x2 (.y(sum), .a(AXorB), .b(cin));
+    and (AAndB, a, b);
+    and (AXorB_And_Cin, AXorB, cin);
+    or (cout, AAndB, AXorB_And_Cin);
+endmodule
+
+
+module adder16(
+    input [15:0] a,
+    input [15:0] b,
+    input cin,
+    output [15:0] sum,
+    output cout
+    );
+    
+    wire [15:0] c;
+    
+    adder adder_0 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c[0]));
+    
+    genvar i;
+    generate
+        for (i = 1; i < 16; i = i + 1) begin
+            adder adder_i (.a(a[i]), .b(b[i]), .cin(c[i-1]), .sum(sum[i]), .cout(c[i]));
+        end
+    endgenerate
+    
+    assign cout = c[15];
+ 
+endmodule
+
+module increment(
+    input [15:0] a,
+    output [15:0] out
+    );
+    wire co;
+    adder16 inc (.a(a), .b(1), .cin(0), .sum(out), .cout(co));
+endmodule
+
+module decrement(
+    input [15:0] a,
+    output [15:0] out
+    );
+    wire co;
+    adder16 dec (.a(a), .b(-1), .cin(0), .sum(out), .cout(co));
+endmodule
+    
+module invert16(
+    input [15:0] a,
+    output [15:0] out
+    );
+    
+    wire [15:0] notA;
+
+    genvar i;
+    generate
+        for (i = 0; i < 16; i = i + 1) begin
+            not (notA[i], a[i]);
+        end
+    endgenerate
+    
+    increment inc (.a(notA), .out(out));
+    
+endmodule
+
+module subtract16(
+    input [15:0] a,
+    input [15:0] b,
+    output [15:0] out
+    );
+    
+    wire [15:0] negB;
+    wire co;
+    
+    invert16 inv (.a(b), .out(negB));
+    adder16 sub (.a(a), .b(negB), .cin(0), .sum(out), .cout(co));
+    
+endmodule
+
+    
+module or16(
+    input [15:0] a,
+    input [15:0] b,
+    output [15:0] out
+    );
+    
+    genvar i;
+    generate
+        for (i = 0; i < 16; i = i + 1) begin
+            or (out[i], a[i], b[i]);
+        end
+    endgenerate
+    
+endmodule
+
+module and16(
+    input [15:0] a,
+    input [15:0] b,
+    output [15:0] out
+    );
+    
+    genvar i;
+    generate
+        for (i = 0; i < 16; i = i + 1) begin
+            and (out[i], a[i], b[i]);
+        end
+    endgenerate
+    
+endmodule
+
+module sle16(
+    input [15:0] a,
+    input [15:0] b,
+    output out
+    );
+    
+    wire [15:0] sum;
+
+    subtract16 sub (.a(a), .b(b), .cin(0), .sum(sum), .cout(co));
+    
+    assign out = sum[15];
+    
+endmodule
+
+module lshift_stage_1 (
+    input [15:0] in,
+    input enable,
+    output [15:0] out
+);
+    assign out = enable ? {in[14:0], 1'b0} : in;
+endmodule
+
+module lshift_stage_2 (
+    input [15:0] in,
+    input enable,
+    output [15:0] out
+);
+    assign out = enable ? {in[13:0], 2'b0} : in;
+endmodule
+
+
+module lshift_stage_4 (
+    input [15:0] in,
+    input enable,
+    output [15:0] out
+);
+    assign out = enable ? {in[11:0], 4'b0} : in;
+endmodule
+
+module lshift_stage_8 (
+    input [15:0] in,
+    input enable,
+    output [15:0] out
+);
+    assign out = enable ? {in[7:0], 8'b0} : in;
+endmodule
+
+
+module lshiftby(
+    input  [15:0] a,
+    input  [15:0]  b,
+    output [15:0] out
+);
+
+    wire [15:0] stage1_out;
+    wire [15:0] stage2_out;
+    wire [15:0] stage3_out;
+    wire [15:0] stage4_out;
+
+    lshift_stage_1 s1 (
+        .in(a),
+        .enable(b[0]),
+        .out(stage1_out)
+    );
+
+    lshift_stage_2 s2 (
+        .in(stage1_out),
+        .enable(b[1]),
+        .out(stage2_out)
+    );
+
+    lshift_stage_4 s4 (
+        .in(stage2_out),
+        .enable(b[2]),
+        .out(stage3_out)
+    );
+
+    lshift_stage_8 s8 (
+        .in(stage3_out),
+        .enable(b[3]),
+        .out(stage4_out)
+    );
+
+    assign out = stage4_out;
+
+endmodule
+
+
+module rshift_stage_1 (
+    input [15:0] in,
+    input enable,
+    input arithmetic,
+    output [15:0] out
+);
+    assign out = enable ? {arithmetic ? in[15] : 1'b0, 0'b0, in[15:1]} : in;
+endmodule
+
+module rshift_stage_2 (
+    input [15:0] in,
+    input enable,
+    input arithmetic,
+    output [15:0] out
+);
+    assign out = enable ? {arithmetic ? in[15] : 1'b0, 1'b0, in[15:2]} : in;
+endmodule
+
+
+module rshift_stage_4 (
+    input [15:0] in,
+    input enable,
+    input arithmetic,
+    output [15:0] out
+);
+    assign out = enable ? {arithmetic ? in[15] : 1'b0, 3'b0, in[15:4]} : in;
+endmodule
+
+module rshift_stage_8 (
+    input [15:0] in,
+    input enable,
+    input arithmetic,
+    output [15:0] out
+);
+    assign out = enable ? {arithmetic ? in[15] : 1'b0, 7'b0, in[15:8]} : in;
+endmodule
+
+
+module rshiftby(
+    input  [15:0] a,
+    input  [15:0]  b,
+    input arithmetic,
+    output [15:0] out
+);
+
+    wire [15:0] stage1_out;
+    wire [15:0] stage2_out;
+    wire [15:0] stage3_out;
+    wire [15:0] stage4_out;
+
+    rshift_stage_1 s1 (
+        .in(a),
+        .enable(b[0]),
+        .arithmetic(arithmetic),
+        .out(stage1_out)
+    );
+
+    rshift_stage_2 s2 (
+        .in(stage1_out),
+        .enable(b[1]),
+        .arithmetic(arithmetic),
+        .out(stage2_out)
+    );
+
+    rshift_stage_4 s4 (
+        .in(stage2_out),
+        .enable(b[2]),
+        .arithmetic(arithmetic),
+        .out(stage3_out)
+    );
+
+    rshift_stage_8 s8 (
+        .in(stage3_out),
+        .enable(b[3]),
+        .arithmetic(arithmetic),
+        .out(stage4_out)
+    );
+
+    assign out = stage4_out;
+
+endmodule
+
+
+
+//module a_rshift(
+//   input [15:0] a,
+//   output [15:0] out
+//   );
+   
+//   assign out[15] = a[15];
+   
+//   genvar i;
+//   generate
+//       for (i = 0; i < 15; i = i + 1) begin
+//           assign out[i] = a[i + 1];
+//       end
+//   endgenerate
+//endmodule
+
+//module l_rshift(
+//   input [15:0] a,
+//   output [15:0] out
+//   );
+   
+//   assign out[15] = 0;
+   
+//   genvar i;
+//   generate
+//       for (i = 0; i < 15; i = i + 1) begin
+//           assign out[i] = a[i + 1];
+//       end
+//   endgenerate
+//endmodule
+
+
+
+//module m31(
+//    input D0,
+//    input D1,
+//    input D2,
+//    input [1:0] S,
+//    output Y
+//    );
+    
+//    assign Y = S[0] ? D1 : (S[1] ? D2 : D0);
+    
+//endmodule
