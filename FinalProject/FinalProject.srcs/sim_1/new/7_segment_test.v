5ee4411e17d049f8e4b899284faa9134f3d22ac1
Arthur-AZ-Zhou <arthuracct@gmail.com>
2025-11-12 11:00:02 -0800

    Add segment display testbench

---
+++ FinalProject/FinalProject.srcs/sim_1/new/7_segment_test.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/12/2025 10:57:52 AM
+// Design Name: 
+// Module Name: 7_segment_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module segment_display_test(
+    input num,
+    output seg,
+    input clk,
+    output an
+    );
+endmodule
